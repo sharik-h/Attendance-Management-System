d1121e886548b5387796f6507248b3dc92d6514a
Mohammed sharikh <mohammedsharik0123@gmail.com>
2022-11-09 22:22:05 +0530

    removed unncessary file

--- app/src/main/java/com/example/ams/Login/LoginNavigation/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ams.Login.LoginNavigation
-
-sealed class Screen(val route: String) {
-    object LoginPage: Screen(route = "LoginPage")
-    object NewAccountPage: Screen(route = "NewAccountPage")
-}
