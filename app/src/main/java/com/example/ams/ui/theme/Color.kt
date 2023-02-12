1fbc300cb8b9fcfda2e72a7684c9d40be9fee3d0
Mohammed sharikh <mohammedsharik0123@gmail.com>
2023-02-12 08:04:28 +0530

    added new colors to Color.kt

--- app/src/main/java/com/example/ams/ui/theme/Color.kt
+++ app/src/main/java/com/example/ams/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val pri = Color(0xff86A3B8)
+val sec = Color(0xffE8D2A6)
+val tri = Color(0xffF48484)
+val qua = Color(0xffF55050)
