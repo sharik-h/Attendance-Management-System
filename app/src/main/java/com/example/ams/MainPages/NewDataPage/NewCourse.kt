b099f036b19cec30155238b5d199d47ecb5d100d
Mohammed sharikh <mohammedsharik0123@gmail.com>
2023-03-07 06:52:36 +0530

    Updated NewCourse.kt page UI

--- app/src/main/java/com/example/ams/MainPages/NewDataPage/NewCourse.kt
+++ app/src/main/java/com/example/ams/MainPages/NewDataPage/NewCourse.kt
@@ -1,5 +1,6 @@
 package com.example.ams.MainPages
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -10,73 +11,99 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.ams.MainPages.CustomComposes.customDropDown
 import com.example.ams.R
 import com.example.ams.data.ViewModel.FirebaseViewModel
+import com.example.ams.ui.theme.pri
 
 @Composable
 fun NewCourse(
     navHostController: NavHostController,
     viewModel: FirebaseViewModel
 ) {
-    val bungeeStyle = FontFamily(Font(R.font.bungee))
+    val quickSand = FontFamily(Font(R.font.quicksand_medium))
     val newCourseData = viewModel.newCourseData.value
 
-    Column(
-        Modifier
-            .fillMaxSize()
-            .padding(horizontal = 20.dp)
-            .verticalScroll(rememberScrollState())) {
-        Text(
-            text = "Create New course",
-            fontFamily = bungeeStyle,
-            fontSize = 30.sp,
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center
-        )
-        Text( text = "Name", fontFamily = bungeeStyle, fontSize = 15.sp)
-        CustomTextFeild(
-            value = newCourseData.name,
-            onValueChange = { viewModel.updateData("name", it) },
-            isError = viewModel.checkIfNameIsUsed(),
-            errorMessage = "You cannot take this name, It is already taken"
-        )
-        Text(text = "Course name", fontFamily = bungeeStyle, fontSize = 15.sp)
-        CustomTextFeild(
-            value = newCourseData.courseName,
-            onValueChange = { viewModel.updateData("courseName", it) })
-        Text(text = "Batch year", fontFamily = bungeeStyle, fontSize = 15.sp)
-        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-           CustomTextFeild(
-               value = newCourseData.batchFrom,
-               onValueChange = { viewModel.updateData("batchFrom", it) })
-            Text(text = "TO", fontFamily = bungeeStyle)
+    var isExpanded by remember { mutableStateOf(false) }
+    var isExpandedTo by remember { mutableStateOf(false) }
+
+    Column(Modifier.fillMaxSize()) {
+        TopAppBar(backgroundColor = pri) {
+            IconButton(onClick = { navHostController.navigateUp() }) {
+                Image(painter = painterResource(id = R.drawable.arrow_back), contentDescription = "")
+            }
+            Text(
+                text = "New class",
+                fontFamily = quickSand,
+                fontSize = 30.sp,
+                color = Color.White
+            )
+        }
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp)
+                .verticalScroll(rememberScrollState())) {
+            Text( text = "Name", fontFamily = quickSand, fontSize = 17.sp)
             CustomTextFeild(
-                value = newCourseData.batchTo,
-                onValueChange = { viewModel.updateData("batchTo", it) })
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-            Text(text = "No. of attendace per day", fontFamily = bungeeStyle)
-            Spacer(modifier = Modifier.width(20.dp))
+                value = newCourseData.name,
+                onValueChange = { viewModel.updateData("name", it) },
+                isError = viewModel.checkIfNameIsUsed(),
+                errorMessage = "You cannot take this name, It is already taken"
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "Course name", fontFamily = quickSand, fontSize = 17.sp)
             CustomTextFeild(
-                value = newCourseData.noAttendace,
-                onValueChange = { viewModel.updateData("noAttendance", it) })
+                value = newCourseData.courseName,
+                onValueChange = { viewModel.updateData("courseName", it) })
+            Spacer(modifier = Modifier.height(20.dp))
+            Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "Batch :  From", fontFamily = quickSand, fontSize = 17.sp)
+                Spacer(modifier = Modifier.width(10.dp))
+                customDropDown(
+                    isEditEnabled = true,
+                    expanded = isExpandedTo,
+                    batch = newCourseData.batchFrom
+                ) {
+                    isExpandedTo = !isExpandedTo
+                    newCourseData.batchFrom = it
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(text = "To", fontFamily = quickSand, fontSize = 17.sp)
+                Spacer(modifier = Modifier.width(10.dp))
+                customDropDown(
+                    isEditEnabled = true,
+                    expanded = isExpanded,
+                    batch = newCourseData.batchTo,
+                ){
+                    isExpanded = !isExpanded
+                    newCourseData.batchTo = it
+                }
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "No. of attendace per day", fontFamily = quickSand)
+                Spacer(modifier = Modifier.width(20.dp))
+                CustomTextFeild(
+                    value = newCourseData.noAttendace,
+                    onValueChange = { viewModel.updateData("noAttendance", it) })
+            }
+            Spacer(modifier = Modifier.weight(0.9f))
+            Button(
+                onClick = { if(viewModel.checkAndCreateClass()) navHostController.popBackStack() },
+                modifier = Modifier
+                    .height(40.dp)
+                    .fillMaxWidth(),
+                contentPadding = PaddingValues(2.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = pri)
+            ) {
+                Text(text = "Save Course", fontFamily = quickSand, color = Color.White)
+            }
+            Spacer(modifier = Modifier.weight(0.1f))
         }
-        Spacer(modifier = Modifier.weight(0.9f))
-        Button(
-            onClick = { if(viewModel.checkAndCreateClass()) navHostController.popBackStack() },
-            modifier = Modifier
-                .height(40.dp)
-                .fillMaxWidth(),
-            contentPadding = PaddingValues(2.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black)
-        ) {
-            Text(text = "Save Course", fontFamily = bungeeStyle, color = Color.White)
-        }
-        Spacer(modifier = Modifier.weight(0.1f))
     }
 }
