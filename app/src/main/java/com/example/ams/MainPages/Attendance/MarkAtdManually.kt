776dd3675be3a9fa3581040e661a9596ffc766a9
Mohammed sharikh <mohammedsharik0123@gmail.com>
2023-03-07 07:19:10 +0530

    Updated MarkAtdManually.kt page UI

--- app/src/main/java/com/example/ams/MainPages/Attendance/MarkAtdManually.kt
+++ app/src/main/java/com/example/ams/MainPages/Attendance/MarkAtdManually.kt
@@ -1,6 +1,7 @@
 package com.example.ams.MainPages
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,12 +13,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.ams.R
 import com.example.ams.data.ViewModel.FirebaseViewModel
+import com.example.ams.ui.theme.pri
 
 @Composable
 fun MarkAtdManually(
@@ -30,24 +35,32 @@ fun MarkAtdManually(
     viewModel.getTotalAtd(adminId = adminId, courseName = courseName)
     viewModel.getAllStudents(adminId = adminId, courseName = courseName)
     val studentDetail by viewModel.studentList.observeAsState(initial = emptyList())
-
-    val arrowBackIcon = painterResource(id = R.drawable.arrow_back)
-    val checkIconWhite = painterResource(id = R.drawable.check_mark_white)
+    val quickSand = FontFamily(Font(R.font.quicksand_medium))
+    var even by remember { mutableStateOf(false) }
 
     Column(Modifier.fillMaxSize()) {
-        TopAppBar {
+        TopAppBar(backgroundColor = pri) {
             IconButton(onClick = { navHostController.navigateUp() }) {
-                Icon(painter = arrowBackIcon, contentDescription = "")
+                Image(painter = painterResource(id = R.drawable.arrow_back), contentDescription = "")
             }
+            Text(
+                text = "Mark Attendance",
+                fontFamily = quickSand,
+                color = White,
+                fontSize = 20.sp
+            )
         }
         LazyColumn {
             items(items = studentDetail){
-                StudentAttendanceMarker(name = it){ viewModel.addAtdData(it) }
+                StudentAttendanceMarker(name = it, even = even){ viewModel.addAtdData(it) }
+                even = !even
             }
         }
     }
     Column(
-        modifier = Modifier.fillMaxSize().padding(20.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
         verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.End
     ) {
@@ -55,29 +68,35 @@ fun MarkAtdManually(
             onClick = {
                 viewModel.markAttendance(adminId = adminId, courseName = courseName)
                 navHostController.navigateUp() },
-            backgroundColor = Color.Black
+            backgroundColor = pri
         ) {
-            Image(painter = checkIconWhite, contentDescription = "")
+            Image(painter = painterResource(id = R.drawable.check_mark_white), contentDescription = "")
         }
     }
 }
 
 @Composable
-fun StudentAttendanceMarker(name: String, onClick: () -> Unit) {
+fun StudentAttendanceMarker(name: String, even: Boolean, onClick: () -> Unit) {
     var checked by remember { mutableStateOf(false) }
+    val quickSand = FontFamily(Font(R.font.quicksand_medium))
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .height(36.dp)
-            .padding(start = 10.dp, top = 0.dp)
+            .background(if (even) Color(0xFFEEEEEE) else White)
+            .padding(start = 10.dp)
             .clickable {
                 checked = !checked
-                onClick() },
-        verticalAlignment = Alignment.CenterVertically
+                onClick()
+            },
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Text(text = name, fontSize = 20.sp, modifier = Modifier
-            .weight(0.7f)
-            .padding(top = 10.dp))
+        Text(
+            text = name,
+            fontSize = 20.sp,
+            fontFamily = quickSand,
+            modifier = Modifier.weight(0.7f)
+        )
         Spacer(modifier = Modifier.width(5.dp) )
         Checkbox(
             checked = checked,
