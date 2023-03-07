fba29157941fe355e3a22c611d7dbc7727024b1a
Mohammed sharikh <mohammedsharik0123@gmail.com>
2023-03-07 07:02:58 +0530

    Updated ImportCourse.kt page UI

--- app/src/main/java/com/example/ams/MainPages/NewDataPage/ImportCourse.kt
+++ app/src/main/java/com/example/ams/MainPages/NewDataPage/ImportCourse.kt
@@ -1,14 +1,14 @@
 package com.example.ams.MainPages.NewDataPage
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -17,38 +17,55 @@ import com.example.ams.data.ViewModel.FirebaseViewModel
 import androidx.navigation.NavHostController
 import com.example.ams.MainPages.CustomTextFeild
 import com.example.ams.R
+import com.example.ams.ui.theme.pri
 
 @Composable
 fun ImportCourse(navHostController: NavHostController  ,viewModel: FirebaseViewModel) {
 
     val requestData = viewModel.requestData.value
-    val bungeeStyle = FontFamily(Font(R.font.bungee))
+    val quickSand = FontFamily(Font(R.font.quicksand_medium))
 
-    Column(modifier = Modifier.fillMaxSize().padding(start = 20.dp, end = 20.dp, bottom = 20.dp)) {
-        Text(text = "Import a class", fontFamily = bungeeStyle, fontSize = 25.sp)
-        Text(text = "Name of class", fontFamily = bungeeStyle, fontSize = 15.sp)
-        CustomTextFeild(
-            value = requestData.ClassName,
-            onValueChange = { viewModel.updateData("importClassName", it) }
-        )
-        Text(text = "creater Phone", fontFamily = bungeeStyle, fontSize = 15.sp)
-        CustomTextFeild(
-            value = requestData.AdminPhone,
-            onValueChange = { viewModel.updateData("creatorPhoneNo", it) }
-        )
-        Spacer(modifier = Modifier.weight(0.5f))
-        Button(
-            onClick = {
-                viewModel.checkRequestDetails()
-                navHostController.navigateUp()
-            },
+    Column(Modifier.fillMaxSize()) {
+        TopAppBar(backgroundColor = pri) {
+            IconButton(onClick = { navHostController.navigateUp() }) {
+                Image(painter = painterResource(id = R.drawable.arrow_back), contentDescription = "")
+            }
+            Text(
+                text = "Import a class",
+                fontFamily = quickSand,
+                fontSize = 30.sp,
+                color = Color.White
+            )
+        }
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(20)),
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black),
+                .fillMaxSize()
+                .padding(20.dp)
         ) {
-            Text(text = "Save New student", fontFamily = bungeeStyle, color = Color.White)
+            Text(text = "Name of class", fontFamily = quickSand, fontSize = 17.sp)
+            CustomTextFeild(
+                value = requestData.ClassName,
+                onValueChange = { viewModel.updateData("importClassName", it) }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "Admin phone", fontFamily = quickSand, fontSize = 17.sp)
+            CustomTextFeild(
+                value = requestData.AdminPhone,
+                onValueChange = { viewModel.updateData("creatorPhoneNo", it) }
+            )
+            Spacer(modifier = Modifier.weight(0.5f))
+            Button(
+                onClick = {
+                    viewModel.checkRequestDetails()
+                    navHostController.navigateUp()
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(20)),
+                colors = ButtonDefaults.buttonColors(backgroundColor = pri),
+            ) {
+                Text(text = "Save New student", fontFamily = quickSand, color = Color.White)
+            }
         }
     }
-
 }
