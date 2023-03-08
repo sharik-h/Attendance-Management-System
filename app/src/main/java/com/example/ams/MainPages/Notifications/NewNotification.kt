1a5b6706f08d12bd1958fb086211a57773d67945
Mohammed sharikh <mohammedsharik0123@gmail.com>
2023-03-08 12:57:33 +0530

    Updated NewNotification.kt page UI

--- app/src/main/java/com/example/ams/MainPages/Notifications/NewNotification.kt
+++ app/src/main/java/com/example/ams/MainPages/Notifications/NewNotification.kt
@@ -1,7 +1,6 @@
 package com.example.ams.MainPages.Notifications
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,13 +11,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.ams.R
 import com.example.ams.data.ViewModel.FirebaseViewModel
 import java.time.LocalDate
-import java.time.LocalTime
+import com.example.ams.ui.theme.pri
 
 @Composable
 fun NewNotification(
@@ -26,16 +29,20 @@ fun NewNotification(
     courseName: String,
     viewModel: FirebaseViewModel
 ){
-
-    val backArrow = painterResource(id = R.drawable.arrow_back)
+    val quickSand = FontFamily(Font(R.font.quicksand_medium))
     val notificationData = viewModel.notificationData.value
 
     Column(Modifier.fillMaxSize()) {
-        TopAppBar {
+        TopAppBar(backgroundColor = pri) {
             IconButton(onClick = { navHostController.navigateUp() }) {
-                Image(painter = backArrow, contentDescription = "")
+                Image(painter = painterResource(id = R.drawable.arrow_back), contentDescription = "")
             }
-            Text(text = "New Notification", color = Color.White)
+            Text(
+                text = "New Notification",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontFamily = quickSand
+            )
             Spacer(modifier = Modifier.weight(0.1f))
             TextButton(onClick = {
                 if (notificationData.notificationId == "") {
@@ -45,7 +52,12 @@ fun NewNotification(
                 }
                 navHostController.navigateUp()
             }) {
-                Text(text = "Save", color = Color.White)
+                Text(
+                    text = "Save",
+                    color = Color.White,
+                    fontFamily = quickSand,
+                    fontSize = 20.sp
+                )
             }
         }
 
@@ -54,19 +66,26 @@ fun NewNotification(
             onValueChange = { viewModel.updateData(name = "notificationHeading", value = it) },
             maxLines = 3,
             keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-            placeholder = { Text(text = "Title goes here", color = Color.Gray) },
+            placeholder = { Text(text = "Title goes here", color = Color.Gray, fontFamily = quickSand) },
+            textStyle = TextStyle(fontFamily = quickSand),
             colors = TextFieldDefaults.textFieldColors(
                 focusedIndicatorColor = Color.Transparent,
                 backgroundColor = Color.Transparent,
                 unfocusedIndicatorColor = Color.Transparent
             )
         )
-        Text(text = LocalDate.now().toString(), modifier = Modifier.padding(start = 15.dp))
+        Text(
+            text = LocalDate.now().toString(),
+            modifier = Modifier.padding(start = 15.dp),
+            fontFamily = quickSand,
+            fontSize = 17.sp
+        )
         TextField(
             value = notificationData.discription,
             onValueChange = { viewModel.updateData(name = "notificationDiscrip", value = it) },
-            placeholder = { Text(text = "Give some details about it", color = Color.Gray) },
+            placeholder = { Text(text = "Give some details about it", color = Color.Gray, fontFamily = quickSand) },
             modifier = Modifier.fillMaxSize(),
+            textStyle = TextStyle(fontFamily = quickSand),
             colors = TextFieldDefaults.textFieldColors(
                 focusedIndicatorColor = Color.Transparent,
                 backgroundColor = Color.Transparent,
@@ -74,5 +93,4 @@ fun NewNotification(
             )
         )
     }
-
 }
